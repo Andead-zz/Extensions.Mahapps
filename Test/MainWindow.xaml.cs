a3562bc897b1614a9bcf3717c980df57234882f8
Андрей Кондратов <andrew.kondratov@gmail.com>
2015-12-24 02:01:46 +0300

    First control: scrollable window.

---
+++ Test/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Test
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            new SampleWindow().ShowDialog();
+        }
+    }
+}
---
+++ Test/SampleWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Test
+{
+    /// <summary>
+    ///     Interaction logic for SampleWindow.xaml
+    /// </summary>
+    public partial class SampleWindow
+    {
+        public SampleWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
